51239e4c34c18623c82d971180043809c641783c
WinterTF <WinterTF2@gmail.com>
2026-02-02 11:33:54 +0100

    Added build step to compile custom css

--- rebuild.sh
+++ rebuild.sh
@@ -2,4 +2,13 @@
 ./vendor/bin/sail up -d
 sleep 5
 ./vendor/bin/sail artisan migrate
+
+# Build CSS assets
+npm run build
+
+# Clear caches
+./vendor/bin/sail artisan view:clear
+./vendor/bin/sail artisan cache:clear
+./vendor/bin/sail artisan config:clear
+
 php artisan serve
