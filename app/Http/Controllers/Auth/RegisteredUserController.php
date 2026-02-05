053ce9ff4796c7b3a06a79bd053ebf56fe12fbf6
marlon0205 <dev.marlon@proton.me>
2026-02-05 10:05:35 +0100

    Set default `is_active` to false for new user registrations and add `is_active` to User model's fillable attributes.

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -38,6 +38,7 @@ class RegisteredUserController extends Controller
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
+            'is_active' => false,
             'password' => Hash::make($request->password),
         ]);
 
--- app/Models/User.php
+++ app/Models/User.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable implements MustVerifyEmail {
+    class User extends Authenticatable implements MustVerifyEmail {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
@@ -20,6 +20,7 @@ class User extends Authenticatable implements MustVerifyEmail {
     protected $fillable = [
         'name',
         'email',
+        'is_active',
         'password',
     ];
 
